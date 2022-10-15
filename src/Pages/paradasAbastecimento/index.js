18269e9103acaf6a29b127cd1a7739ec0f85a7b5
Arthur Miguel <arthurmiguel241006@gmail.com>
2022-10-15 18:21:46 -0300

    alt prd abastr

---
+++ src/Pages/paradasAbastecimento/index.js
@@ -0,0 +1,25 @@
+import './index.scss'
+import React, { useState } from 'react';
+
+export default function ParadasAbastecimento() {
+    const[capac, setCapac] = useState(0);
+    const[consumo,setConsumo] = useState(0);
+    const[dist, setDist] = useState(0);
+
+    function paradasAbastecimento() {
+    }
+
+    return(
+        <main>
+
+            <section>
+
+                <h1> Bem vindo a Paradas Abastecimento! </h1>
+
+                <h2> Capacidade </h2>
+
+            </section>
+
+        </main>
+    )
+}
